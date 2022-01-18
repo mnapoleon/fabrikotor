c8f618cb7204351c4ae84907ed94856c8b74dca1
Michael Napoleon <michael.napoleon@gmail.com>
2022-01-17 19:54:36 -0500

    changes

--- src/main/kotlin/org/napoloen/fabrikotor/Calendar.kt
+++ src/main/kotlin/org/napoloen/fabrikotor/Calendar.kt
@@ -1,5 +1,7 @@
 package org.napoloen.fabrikotor
 
+import org.joda.time.DateTime
+import org.joda.time.IllegalFieldValueException
 import kotlin.random.Random
 
 class Calendar(private val alphanumeric: Alphanumeric,
@@ -12,5 +14,48 @@ class Calendar(private val alphanumeric: Alphanumeric,
         return if (randomVal < 5) "am" else "pm"
     }
 
+    fun day(): String = day(year().toInt(), month()?.toInt() ?: 0, 1, 31)
+
+    fun day(year: Int, month: Int): String = day(year, month, 1, 31)
+
+    private fun day(year: Int, month: Int, min: Int, max: Int): String {
+        if ((min <=0 || max >= 31) || (min >= 31 || max <= 0)) throw IllegalArgumentException("min and max values should be in [1,31] range")
+        var result = ""
+        var dayValue = 0
+        while(result.equals("")) {
+            try {
+                dayValue = alphanumeric.randomInt(min, max)
+                val date = DateTime(year, month, dayValue, 0, 0)
+                result = dayValue.toString()
+            }
+            catch(e: IllegalFieldValueException) {
+
+            }
+        }
+        return result
+    }
+
     fun dayOfWeek(): String = utilityService?.getValueFromJsonArray("day_of_week") ?: ""
+
+    fun time24h(): String = hour24h() + ":" + minute()
+
+    fun time12h(): String = hour12h() + ":" + minute()
+
+    fun hour24h(): String = alphanumeric.randomInt(0,24).toString()
+
+    fun hour12h(): String = alphanumeric.randomInt(0, 12).toString()
+
+    fun second(): String = alphanumeric.randomInt(0, 59).toString()
+
+    fun minute(): String = alphanumeric.randomInt(0, 59).toString()
+
+    fun month(): String? = month(asNumber = true)
+
+    private fun month(asNumber: Boolean): String? =
+        if (asNumber) alphanumeric.randomInt(1, 12).toString()
+        else utilityService.getValueFromJsonArray("month")
+
+    fun year(): String = alphanumeric.randomInt(1970, 2055).toString()
+
+    fun century(): String? = utilityService.getValueFromJsonArray("centuries")
 }
--- src/test/kotlin/org/napoleon/fabrikotor/CalendarTestSuite.kt
+++ src/test/kotlin/org/napoleon/fabrikotor/CalendarTestSuite.kt
@@ -4,7 +4,7 @@ import org.testng.annotations.*
 
 class CalendarTestSuite: BaseTestSuite() {
 
-    val daysOfWeekList = util.getArrayFromJson("day_of_week")
+    private val daysOfWeekList = util.getArrayFromJson("day_of_week")
 
     @Test
     fun testAmPm() {
@@ -15,6 +15,41 @@ class CalendarTestSuite: BaseTestSuite() {
             if (ampm == "am") amCount += 1 else pmCount += 1
         }
         assert(amCount > 0 && pmCount > 0)
+    }
+
+    @Test
+    fun testSecond() {
+        val second = calendar.second()
+        assert(second.toInt() in 0..59)
+    }
+
+    @Test
+    fun testMinute() {
+        val minute = calendar.minute()
+        assert(minute.toInt() in 0 .. 59)
+    }
+
+    @Test
+    fun testHour() {
+        val hour24 = calendar.hour24h()
+        val hour12 = calendar.hour12h()
+        assert(hour24.toInt() in 0 .. 23)
+        assert(hour12.toInt() in 0 .. 11)
+    }
+
+    @Test
+    fun testDefaultDay() {
+        val day = calendar.day().toInt()
+        print("DAY is $day")
+        assert(day in 1 .. 31)
+    }
+
+    @Test
+    fun testDay() {
+        val year = calendar.year()
+        val month = calendar.month()
+        val day = calendar.day(year.toInt(), month?.toInt()?:0)
+        assert(day.toInt() in 0 .. 31)
     }
 
     @Test
